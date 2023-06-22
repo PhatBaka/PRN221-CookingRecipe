99f19a94e28d7fcba10ae70f2cd85d1d51cda0a3
Hung <lnhungtn789@gmail.com>
2023-06-22 22:18:40 +0700

    post recipe page
    note: need better css, js

---
+++ Recipez/Pages/PostRecipe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Recipez.Pages
+{
+    public class PostRecipeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
